60cdbc61d117977db9955aa27f88bfe77f918544
GitHub Action <action@github.com>
2025-04-05 13:26:51 +0000

    Template cleanup

--- src/main/kotlin/com/github/dragos0064/codemapai/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/dragos0064/codemapai/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.dragos0064.codemapai.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
